62a407a4bf98a0e6d633e39e3962d48420f74a13
Yan Kaiky <yankaikys@gmail.com>
2024-03-04 23:23:45 -0300

    feat(web.php): add routes

---
+++ resources/views/products.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title', 'Products')
+@section('content')
+
+<h1>Products screen</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name  = "Yan";
+    $age  = 21;
+    $job = "Full Stack Developer & Mobile";
+    $array = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+    $names = ["Yan", "Kaiky", "Augusto", "Miles", "Carlos", "Eliza", "Jacira"];
+
+    return view('welcome', [
+        "name" => $name,
+        "age" => $age,
+        "job" => $job,
+        "array" => $array,
+        "names" => $names
+    ]);
+});
+
+Route::get('/products', function () {
+    return view('products');
 });
